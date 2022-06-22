a3221b0c4aff4bd7fcdb16e95f025ee4f19abb04
venkat <venky.sdl55@gmail.com>
2022-06-22 15:03:04 +0530

    First commit

---
+++ src/sample/parentclass.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class parentclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("my git code 1");
+
+	}
+
+}
